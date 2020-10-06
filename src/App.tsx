e2f2bbde3d2061d812b9cdea813d85431f64dc00
Tolu Adesina <tolu@Tolus-MacBook-Pro.local>
2020-10-06 15:24:56 +0100

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+
+const Home = lazy(() => import('./views/Home'))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback="loading...">
+      <Router>
+        <Route exact path='/' component={Home} />
+      </Router>
+    </Suspense>
   );
 }
 
