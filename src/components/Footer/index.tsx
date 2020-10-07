f0b503bc09541ffab9071f5ea9b47f046cb89108
Tolu Adesina <tolu@Tolus-MacBook-Pro.local>
2020-10-07 01:36:18 +0100

    create Footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.scss';
+
+const Footer = () => {
+    return (
+        <footer className="section-center-middle">
+            <div className="container section-center-middle">
+                <img src="/assets/images/wedding-logo.png" alt="wedding logo" />
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
