29190eaee0e0c137da46433284a2d8ca28ca718c
Tolu Adesina <tolu@Tolus-MacBook-Pro.local>
2020-10-07 01:37:09 +0100

    update event page

---
+++ src/views/Event/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './index.scss';
+
+import Wedding from '../../layout/Wedding';
+
+const Event = () => {
+    return (
+        <Wedding>
+            <div className="main">
+                <div className="wedding-banner">
+                    <img src="/assets/images/wedding-banner.png" alt="wedding banner" />
+                </div>
+                <div className="container">
+                    
+                </div>
+            </div>
+        </Wedding>
+    )
+}
+
+export default Event;
