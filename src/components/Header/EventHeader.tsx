98c1a604872ffa8632091980270e7990031d0928
Tolu Adesina <tolu@Tolus-MacBook-Pro.local>
2020-10-07 01:36:32 +0100

    Update Event Header component

--- src/components/Header/EventHeader.tsx
+++ src/components/Header/EventHeader.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import './index.scss';
 import Nav from '../Nav';
 
 interface ComponentProps extends React.PropsWithChildren<unknown> { }
 
 const EventHeader: React.FC<ComponentProps> = (props) => {
     return (
-        <div className="header">
-            <div className="top-header">
-                
+        <header className="section-center-middle">
+            <div className="container section-center-middle">
+                <div className="top-header justify-space">
+                    <div className="side-item">
+                        <p>Link Up</p>
+                    </div>
+                    <div className="wedding-logo">
+                        <img src="/assets/images/wedding-logo.png" alt="wedding" />
+                    </div>
+                    <div className="side-item">
+                        <p>#jolatunji2020</p>
+                    </div>
+                </div>
+                <Nav />
             </div>
-            <p>This is the Event Header</p>
-        </div>
+        </header>
     )
 }
 
