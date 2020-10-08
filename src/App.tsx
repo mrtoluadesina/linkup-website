5c892be6556890cfb3e621d59ba3b54949dda11d
Tolu Adesina <tolu@Tolus-MacBook-Pro.local>
2020-10-08 16:40:48 +0100

    rework routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.scss';
 
 const Home = lazy(() => import('./views/Home'));
-const Event = lazy(() => import('./views/Event'));
-const BigDay = lazy(() => import('./views/BigDay'));
-const Gifts = lazy(() => import('./views/Gifts'));
+const EventRoutes = lazy(() => import('./views/routes'));
 
 function App() {
   return (
     <Suspense fallback="loading...">
       <Router>
         <Route exact path='/' component={Home} />
-        <Route path='/event' component={Event} />
-        <Route path='/big-day' component={BigDay} />
-        <Route path='/gifts' component={Gifts} />
+        <Route path='/event' component={EventRoutes} />
       </Router>
     </Suspense>
   );
