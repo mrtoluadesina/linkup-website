528c02469c5d91982e6912b241bed5aa9fe97271
Tolu Adesina <tolu@Tolus-MacBook-Pro.local>
2020-10-08 05:50:57 +0100

    Update Home with new Slides

--- src/views/Home/Slide.tsx
+++ src/views/Home/Slide.tsx
@@ -4,20 +4,25 @@ import './index.scss';
 import { Link } from 'react-router-dom';
 
 interface ComponentProps extends React.HtmlHTMLAttributes<unknown> {
-    tagline: string,
-    caption: string,
     date: string,
     link: string,
+    slide: number,
 }
 
 const Slide: React.FC<ComponentProps> = (props) => {
     return (
         <div className="slide">
-            <h2>{props.caption}</h2>
-            <p>{props.tagline}</p>
-            <p>Experience the first virtual owambe party</p>
-            <p>Date: {props.date}</p>
-            <Link to={props.link} className="link">Request Invite</Link>
+            <div className="slide-bg">
+                <img src={`/assets/images/home-banner-${props.slide}.jpg`} alt="banner" />
+            </div>
+            <div className="container slide-content">
+                {props.children}
+                <div className="cta section-center-middle">
+                    <p>Experience the first virtual owambe party</p>
+                    <p>Date: {props.date}</p>
+                    <Link to={props.link} className="btn link">Request Invite</Link>
+                </div>
+            </div>
         </div>
     )
 }
