c1197840b2de1d23dbdf6dfa342c8f4496ba230e
Tolu Adesina <tolu@Tolus-MacBook-Pro.local>
2020-10-08 05:50:24 +0100

    update the Header for home header special markup and styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './index.scss';
 
 interface ComponentProps extends React.PropsWithChildren<unknown> { }
 
 const Header: React.FC<ComponentProps> = (props) => {
     return (
-        <header className="section-center-middle">
-            <div className="container section-center-middle">
-                <div className="side-item">
-                    <p>Link Up</p>
+        <header className="home section-center-middle">
+            <div className="container justify-space">
+                <div className="logo-item">
+                    <p>Owambe.ng</p>
                 </div>
-                <div className="side-item">
-                    <p>#jolatunji2020</p>
+                <div className="menu">
+                    <Link to="/">Join Event</Link>
+                    <Link to="/" className="btn">Create Event</Link>
                 </div>
             </div>
         </header>
