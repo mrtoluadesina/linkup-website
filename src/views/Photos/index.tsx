539f5334b701bcde386737e9ea8fc60c88a48b39
Tolu Adesina <tolu@Tolus-MacBook-Pro.local>
2020-10-09 07:36:50 +0100

    add new images for photos page and change import in component view

--- src/views/Photos/index.tsx
+++ src/views/Photos/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Wedding from "../../layout/Wedding";
-import weddingpic from "../../images/wedding-pic.png";
-import weddingpic2 from "../../images/wedding-pic2.png";
-import weddingpic3 from "../../images/weddingpic3.png";
-import weddingpic4 from "../../images/weddingpic4.png";
-import weddingpic5 from "../../images/wedding-pic5.png";
-import weddingpic6 from "../../images/weddingpic6.png";
-import weddingpic7 from "../../images/wedding-pic7.png";
+import weddingpic from "../../images/wedding-pic.jpg";
+import weddingpic2 from "../../images/wedding-pic2.jpg";
+import weddingpic3 from "../../images/wedding-pic3.jpg";
+import weddingpic4 from "../../images/wedding-pic4.jpg";
+import weddingpic5 from "../../images/wedding-pic5.jpg";
+import weddingpic6 from "../../images/wedding-pic6.jpg";
+import weddingpic7 from "../../images/wedding-pic7.jpg";
+import weddingpic8 from "../../images/wedding-pic8.jpg";
 
 
 import "./index.scss";
@@ -38,7 +39,7 @@ const Photos = () => {
             <img src={weddingpic7} alt="" />
           </div>
           <div className="horizontal">
-            <img src={weddingpic6} alt="" />
+            <img src={weddingpic8} alt="" />
           </div>
         </div>
       </div>
