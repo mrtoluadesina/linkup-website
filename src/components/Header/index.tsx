4379491fe548e28546cf4ebb9d49cf232a4014a9
Tolu Adesina <tolu@Tolus-MacBook-Pro.local>
2020-10-20 11:30:31 +0100

    make modal for getting hashtag to query

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Button from '../Button';
+import Input from '../Input';
+import Modal from '../Modal';
 import './index.scss';
 
-interface ComponentProps extends React.PropsWithChildren<unknown> { }
+import axios from 'axios';
+
+interface ComponentProps extends React.PropsWithChildren<unknown> {
+    history?: Array<string>
+}
 
 const Header: React.FC<ComponentProps> = (props) => {
+    const [loading, setLoading] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+    const [hashtag, setHashtag] = useState('');
+
+    const handleClick = (e: any) => {
+        e.preventDefault();
+        setShowModal(true);
+    }
+
+    const closeModal = (e: any) => {
+        e.preventDefault();
+        setShowModal(false);
+    }
+
+    const handleChange = (e: any) => {
+        setHashtag(e.target.value);
+    }
+
+    const handleSubmit = async (e: any) => {
+        setLoading(true);
+        e.preventDefault();
+        try {
+            let response = await axios.get(`https://link-event.herokuapp.com/api/v1/event?hashtag=${hashtag}`);
+            let { data } = response;
+            console.log(response.data);
+            localStorage.setItem('event', JSON.stringify(data));
+            // window.location.replace('/event');
+            window.location.href = '/event';
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
-        <header className="home section-center-middle">
-            <div className="container justify-space">
-                <div className="logo-item">
-                    <Link to="/"><img src="/assets/images/logo-alt.png" alt="brand" /></Link>
+        <>
+            <header className="home section-center-middle">
+                <div className="container justify-space">
+                    <div className="logo-item">
+                        <Link to="/"><img src="/assets/images/logo-alt.png" alt="brand" /></Link>
+                    </div>
+                    <div className="menu">
+                        {/* <Link to="/">Join Event</Link> */}
+                        <a href="/" className="btn btn-white" onClick={handleClick}>Join Event</a>
+                        <a href="https://app.owambe.ng" className="btn">Create Event</a>
+                        {/* <a href="https://app.owambe.ng/event/detail/5f819776dfe9b500251ba716" className="btn">Attend First Event</a> */}
+                    </div>
                 </div>
-                <div className="menu">
-                    {/* <Link to="/">Join Event</Link> */}
-                    {/* <a href="https://linkup-app.netlify.app" className="btn">Create Event</a> */}
-                    <a href="https://app.owambe.ng/event/detail/5f819776dfe9b500251ba716" className="btn">Attend First Event</a>
-                </div>
-            </div>
-        </header>
+            </header>
+            {showModal && <Modal onClick={closeModal}>
+                <form className="form" onSubmit={handleSubmit}>
+                    <Input type="text" name="hashtag" placeholder="event tag" onChange={handleChange} value={hashtag} />
+                    <Button type="submit" text="Proceed" loading={loading} />
+                </form>
+            </Modal>}
+        </>
     )
 }
 
